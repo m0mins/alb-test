8d286eb955c939d4c6874c5ba20624c63c13d1d6
momin <momin@strativ.se>
2025-08-05 18:15:09 +0600

    deleted account and home directory

--- App_Home/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from App_Home.models import Slider
-# Register your models here.
-
-admin.site.register(Slider)
--- App_Home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from App_Home import views
-
-
-app_name='App_Home'
-
-urlpatterns = [
-    path('',views.home ,name='home'),
-]
