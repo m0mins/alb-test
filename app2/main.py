beb60c3340460b07afa919d4ff79632f79c739fe
momin <momin@strativ.se>
2025-08-13 13:58:49 +0600

    updated container response

--- app2/main.py
+++ app2/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from App 2"}
+    return {"message": "Hello from Server 2 App 2"}
--- app3/main.py
+++ app3/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from App 3"}
+    return {"message": "Hello from Server 2 App 3"}
